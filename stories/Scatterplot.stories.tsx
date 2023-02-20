bf733029ade71d4f6cc4001e72279203b6a7515d
Sejmou <samo.kolter@gmail.com>
2023-02-20 16:40:38 +0100

    working scatterplot demo :)

--- stories/Scatterplot.stories.tsx
+++ stories/Scatterplot.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import Scatterplot, { AxisConfig, Props } from '../src';
+import '../tailwind.css';
 
 const defaultXAxisConfig: AxisConfig = {
   data: [3, 1, 2, 4, 5, 8, 9, 10],
@@ -8,7 +9,7 @@ const defaultXAxisConfig: AxisConfig = {
 };
 
 const defaultYAxisConfig: AxisConfig = {
-  data: [5, 15, 25, 3, 12, 16, 18, 7, 2],
+  data: [5, 15, 25, 3, 12, 16, 18, 7],
   featureName: 'Bla',
 };
 
@@ -30,7 +31,11 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => <Scatterplot {...args} />;
+const Template: Story<Props> = args => (
+  <div className="w-96 h-128">
+    <Scatterplot {...args} />
+  </div>
+);
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
