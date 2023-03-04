54374b7718688b75e53ddbd05b4c849d77c999ef
Sejmou <samo.kolter@gmail.com>
2023-03-04 14:49:50 +0100

    add some dumb demo test to make pipeline work without removing tests entirely lol

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,1 +1,13 @@
-// Tests would be here lol
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Default as Scatterplot } from '../stories/Scatterplot.stories';
+
+//TODO: some day add actual tests lol
+
+describe('Scatterplot', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Scatterplot />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
