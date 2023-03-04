bff60ba4b13bb22902f78f97b70817257f492e30
Sejmou <samo.kolter@gmail.com>
2023-03-04 14:39:35 +0100

    remove "test" code that resulted in CI workflows failing

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-// TODO: implement with scatterplot component
-// import { Default as Thing } from '../stories/Thing.stories';
-
-// describe('Thing', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Thing />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// });
+// Tests would be here lol
