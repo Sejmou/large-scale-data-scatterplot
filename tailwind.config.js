2379e27073223caef32156d8a9b334b1a87f86b9
Sejmou <samo.kolter@gmail.com>
2023-03-31 12:04:49 +0200

    now finally fix(?) react-tooltip style import

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  prefix: 'react-big-data-scatterplot-', // add prefix to avoid style conflicts with existing CSS of applications consuming this library
 };
